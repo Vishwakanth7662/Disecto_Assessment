1e2ced15f103585c431d9a578dd5ab6194328e2d
vishwakanth7662 <vishwakanth7662@gmail.com>
2022-02-15 11:33:04 +0530

    changes done to readme.md

--- he.py
+++ he.py
@@ -2,66 +2,25 @@ import numpy as np
 from numpy.polynomial import polynomial as poly
 
 def polymul(x, y, modulus, poly_mod):
-    """Add two polynoms
-    Args:
-        x, y: two polynoms to be added.
-        modulus: coefficient modulus.
-        poly_mod: polynomial modulus.
-    Returns:
-        A polynomial in Z_modulus[X]/(poly_mod).
-    """
     return np.int64(
         np.round(poly.polydiv(poly.polymul(x, y) % modulus, poly_mod)[1] % modulus)
     )
 
 
 def polyadd(x, y, modulus, poly_mod):
-    """Multiply two polynoms
-    Args:
-        x, y: two polynoms to be multiplied.
-        modulus: coefficient modulus.
-        poly_mod: polynomial modulus.
-    Returns:
-        A polynomial in Z_modulus[X]/(poly_mod).
-    """
     return np.int64(
         np.round(poly.polydiv(poly.polyadd(x, y) % modulus, poly_mod)[1] % modulus)
     )
     
 def gen_binary_poly(size):
-    """Generates a polynomial with coeffecients in [0, 1]
-    Args:
-        size: number of coeffcients, size-1 being the degree of the
-            polynomial.
-    Returns:
-        array of coefficients with the coeff[i] being 
-        the coeff of x ^ i.
-    """
     return np.random.randint(0, 2, size, dtype=np.int64)
 
 
 def gen_uniform_poly(size, modulus):
-    """Generates a polynomial with coeffecients being integers in Z_modulus
-    Args:
-        size: number of coeffcients, size-1 being the degree of the
-            polynomial.
-    Returns:
-        array of coefficients with the coeff[i] being 
-        the coeff of x ^ i.
-    """
     return np.random.randint(0, modulus, size, dtype=np.int64)
 
 
 def gen_normal_poly(size):
-    """Generates a polynomial with coeffecients in a normal distribution
-    of mean 0 and a standard deviation of 2, then discretize it.
-    Args:
-        size: number of coeffcients, size-1 being the degree of the
-            polynomial.
-    Returns:
-        array of coefficients with the coeff[i] being 
-        the coeff of x ^ i.
-    """
     return np.int64(np.random.normal(0, 2, size=size))
 
 def keygen(size, modulus, poly_mod):
